c5712fe954a1fb0dcd99b570c91437d24d904624
keaksey <keaksey.sum@gmail.com>
2023-07-01 18:20:46 +0700

    feat: remove localstorage

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import pubsub from "./pubsub";
 export * from "./SaleorAuthClient";
 export * from "./SaleorExternalAuth";
 export * from "./types";
-
+export * from "./react";
 export { pubsub };
--- src/react/index.ts
+++ src/react/index.ts
@@ -3,3 +3,4 @@ export * from "./useSaleorAuthClient";
 export * from "./useAuthChange";
 export * from "./context";
 export * from "./useSaleorExternalAuth";
+export * from "./apollo";
